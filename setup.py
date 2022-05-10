d0fe7243b6257b1f9e4b5cdd86dfc44955bbb139
Vittor Faria Pereira <vittorfpereira@gmail.com>
2022-05-10 15:10:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as fr:
 
 setuptools.setup(
     name="public-hello",
-    version="0.0.4",
+    version="0.0.5",
     author="Nicolas REMOND",
     author_email="remondnicola@gmail.com",
     description="A public github-hosted python package for test, with dependency.",
